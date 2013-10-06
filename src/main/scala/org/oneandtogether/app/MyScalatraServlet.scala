a4d669b16a4054c46adee90715d58a171885dcc8
Matthew Kramer <matthewkram@gmail.com>
2013-10-06 13:45:27 -0400

    added under construction page, and gzip support

--- src/main/scala/org/oneandtogether/app/MyScalatraServlet.scala
+++ src/main/scala/org/oneandtogether/app/MyScalatraServlet.scala
@@ -3,14 +3,22 @@ package org.oneandtogether.app
 import org.scalatra._
 import scalate.ScalateSupport
 
-class MyScalatraServlet extends OneAndTogetherStack {
+class MyScalatraServlet extends OneAndTogetherStack with GZipSupport{
 
   val pages = Array("home", "who", "involved", "news", "donate", "photos", "members", "contact")
   get("/") {
     contentType = "text/html"
-    jade("home.jade")
+    jade("const.jade")
   }
   get("/:id/?") {
+    contentType = "text/html"
+    jade("const.jade")
+    // if (pages.contains(params("id"))) {
+    //   contentType = "text/html"
+    //   jade(params("id") + ".jade")
+    // } else pass()
+  }
+  get("/matt/:id/?") {
     if (pages.contains(params("id"))) {
       contentType = "text/html"
       jade(params("id") + ".jade")
